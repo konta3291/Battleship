d85d81e81bf72748873955e0306f935efa0e66de
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-02 12:10:01 +0900

    ゲームテーブルを表示する処理を追加

---
+++ Battleship/BattleshipGame.vb
@@ -0,0 +1,30 @@
+﻿Public Class BattleshipGame
+
+    ''' <summary>
+    ''' 戦艦ゲームの処理を行う
+    ''' </summary>
+    Public Sub BattleshipGame()
+        ShowGameTable()
+    End Sub
+
+    ''' <summary>
+    ''' ゲームテーブルを表示する
+    ''' </summary>
+    Private Sub ShowGameTable()
+        Console.WriteLine("【BATTLESHIP】")
+
+        '表
+        Console.WriteLine("    A B C D E F G H ")
+        Console.WriteLine("    ________________")
+        For i As Integer = 0 To 7
+            Console.Write("  " & i + 1 & "|")
+            For j As Integer = 0 To 7
+                Console.Write("  ")
+            Next
+            Console.WriteLine("|")
+        Next
+        Console.Write("    ¯¯¯¯¯¯¯¯")
+        Console.ReadLine()
+    End Sub
+
+End Class
--- Battleship/Module1.vb
+++ Battleship/Module1.vb
@@ -1,7 +1,8 @@
 ﻿Module Module1
 
     Sub Main()
-
+        Dim battleshipGame As New BattleshipGame
+        battleshipGame.BattleshipGame()
     End Sub
 
 End Module
