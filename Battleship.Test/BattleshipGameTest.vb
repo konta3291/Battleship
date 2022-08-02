9d6bab12821c9a13c1163ae0c547d39fef1668d5
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-02 13:38:19 +0900

    カーソルの位置を追いかける矢印を追加

---
+++ Battleship.Test/BattleshipGameTest.vb
@@ -0,0 +1,27 @@
+﻿Imports NUnit.Framework
+<TestFixture> Public MustInherit Class BattleshipGameTest
+    Private sut As BattleshipGame
+    <SetUp> Public Overridable Sub Setup()
+        sut = New BattleshipGame
+    End Sub
+
+    Public Class MakeColumnArrowTest : Inherits BattleshipGameTest
+
+        <Test()> Public Sub 矢印を指定の位置で返すか偶数()
+            Dim result As String = sut.MakeColumnArrow(4)
+            Assert.That(result, [Is].EqualTo("    ↓"))
+        End Sub
+        <Test()> Public Sub 矢印を指定の位置で返すか奇数()
+            Dim result As String = sut.MakeColumnArrow(5)
+            Assert.That(result, [Is].EqualTo("    ↓"))
+        End Sub
+
+        <Test()> Public Sub 奇数の場合は1引いた数字と同じ矢印の位置になる()
+            Dim resultA As String = sut.MakeColumnArrow(5)
+            Dim resultB As String = sut.MakeColumnArrow(4)
+            Assert.That(resultA, [Is].EqualTo(resultB))
+        End Sub
+
+    End Class
+
+End Class
--- Battleship/BattleshipGame.vb
+++ Battleship/BattleshipGame.vb
@@ -4,8 +4,8 @@
     ''' 戦艦ゲームの処理を行う
     ''' </summary>
     Public Sub BattleshipGame()
-        ShowGameTable()
-        Console.SetCursorPosition(4, 3)
+        ShowGameTable(4, 4)
+        Console.SetCursorPosition(4, 4)
         Dim isFinish As Boolean = False
         While Not isFinish
             Dim c As ConsoleKeyInfo = Console.ReadKey(True)
@@ -38,14 +38,22 @@
     ''' <summary>
     ''' ゲームテーブルを表示する
     ''' </summary>
-    Private Sub ShowGameTable()
+    Private Sub ShowGameTable(cursorLeft As Integer, cursorTop As Integer)
         Console.WriteLine("【BATTLESHIP】")
+        Dim columnArrow As String = MakeColumnArrow(cursorLeft)
+        Console.WriteLine(columnArrow)
 
         '表
         Console.WriteLine("    A B C D E F G H ")
         Console.WriteLine("    ________________")
         For i As Integer = 0 To 7
-            Console.Write("  " & i + 1 & "|")
+            If cursorTop - 4 = i Then
+                Dim rowArrow As String = "→"
+                Console.Write(rowArrow)
+            Else
+                Console.Write("  ")
+            End If
+            Console.Write(i + 1 & "|")
             For j As Integer = 0 To 7
                 Console.Write("  ")
             Next
@@ -54,6 +62,27 @@
         Console.Write("    ¯¯¯¯¯¯¯¯")
 
     End Sub
+
+    ''' <summary>
+    ''' 今いる列を指す矢印を作る
+    ''' </summary>
+    ''' <param name="currentCursorLeft"></param>
+    ''' <returns></returns>
+    Public Function MakeColumnArrow(currentCursorLeft As Integer) As String
+        Dim columnArrow As String = Nothing
+        Dim cursorLeft As Integer = currentCursorLeft
+        If Not currentCursorLeft Mod 2 = 0 Then
+            cursorLeft -= 1
+        End If
+
+        For i As Integer = 0 To cursorLeft - 1
+            columnArrow &= " "
+        Next
+
+        columnArrow &= "↓"
+        Return columnArrow
+
+    End Function
 
     ''' <summary>
     ''' カーソルを移動させる
@@ -72,14 +101,14 @@
         End If
 
         'テーブルからはみ出そうになったら修正
-        If afterCursorTop < 3 Then
-            afterCursorTop = 3
-        ElseIf 10 < afterCursorTop Then
-            afterCursorTop = 10
+        If afterCursorTop < 4 Then
+            afterCursorTop = 4
+        ElseIf 11 < afterCursorTop Then
+            afterCursorTop = 11
         End If
 
         Console.Clear()
-        ShowGameTable()
+        ShowGameTable(afterCursorLeft, afterCursorTop)
         'カーソルの位置を移動させる
         Console.SetCursorPosition(afterCursorLeft, afterCursorTop)
 
