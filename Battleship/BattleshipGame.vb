7338eb34cb7727641006f7b3dcf911f8d80ba208
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-02 13:23:55 +0900

    ゲームテーブルの中で動くカーソルを追加

--- Battleship/BattleshipGame.vb
+++ Battleship/BattleshipGame.vb
@@ -5,6 +5,34 @@
     ''' </summary>
     Public Sub BattleshipGame()
         ShowGameTable()
+        Console.SetCursorPosition(4, 3)
+        Dim isFinish As Boolean = False
+        While Not isFinish
+            Dim c As ConsoleKeyInfo = Console.ReadKey(True)
+            Select Case c.Key
+
+                Case ConsoleKey.LeftArrow
+                    MoveCursor(-2, 0)
+
+                Case ConsoleKey.RightArrow
+                    MoveCursor(2, 0)
+
+                Case ConsoleKey.UpArrow
+                    MoveCursor(0, -1)
+
+                Case ConsoleKey.DownArrow
+                    MoveCursor(0, 1)
+
+                Case ConsoleKey.Enter
+                    isFinish = True
+
+                Case ConsoleKey.Spacebar
+                    isFinish = True
+
+            End Select
+
+        End While
+
     End Sub
 
     ''' <summary>
@@ -24,7 +52,37 @@
             Console.WriteLine("|")
         Next
         Console.Write("    ¯¯¯¯¯¯¯¯")
-        Console.ReadLine()
+
+    End Sub
+
+    ''' <summary>
+    ''' カーソルを移動させる
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="y"></param>
+    Private Sub MoveCursor(x As Integer, y As Integer)
+        Dim afterCursorLeft As Integer = Console.CursorLeft + x
+        Dim afterCursorTop As Integer = Console.CursorTop + y
+
+        'テーブルからはみ出そうになったら修正
+        If afterCursorLeft < 4 Then
+            afterCursorLeft = 4
+        ElseIf 18 < afterCursorLeft Then
+            afterCursorLeft = 18
+        End If
+
+        'テーブルからはみ出そうになったら修正
+        If afterCursorTop < 3 Then
+            afterCursorTop = 3
+        ElseIf 10 < afterCursorTop Then
+            afterCursorTop = 10
+        End If
+
+        Console.Clear()
+        ShowGameTable()
+        'カーソルの位置を移動させる
+        Console.SetCursorPosition(afterCursorLeft, afterCursorTop)
+
     End Sub
 
 End Class
