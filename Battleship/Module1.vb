d2bee6cfaadae028a9e393bb51d56ad1a26b9247
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-02 15:42:25 +0900

    リプレイできる処理を追加

--- Battleship/Module1.vb
+++ Battleship/Module1.vb
@@ -1,9 +1,26 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim battleshipGame As New BattleshipGame
-        battleshipGame.BattleshipGame()
-        Console.Read()
+        Do
+            Console.Clear()
+            Dim battleshipGame As New BattleshipGame
+            battleshipGame.BattleshipGame()
+        Loop While AskPlayerWantToPlayAgain()
     End Sub
+
+    ''' <summary>
+    ''' もう一度遊ぶか聞きます
+    ''' </summary>
+    Private Function AskPlayerWantToPlayAgain() As Boolean
+        Dim playerAnswer As String
+        Do
+            Console.SetCursorPosition(0, 14)
+            Console.Write("リトライしますか？(Y/N):")
+            playerAnswer = Console.ReadLine()
+        Loop While Not ("Y".Equals(playerAnswer) OrElse "N".Equals(playerAnswer))
+
+        Return playerAnswer.Equals("Y")
+
+    End Function
 
 End Module
