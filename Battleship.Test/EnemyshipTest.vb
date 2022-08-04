0acd30f975a5c1e16031299a9e788a5b1c7bd842
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-04 11:59:37 +0900

    敵船を作る処理を読みやすく変更

---
+++ Battleship.Test/EnemyshipTest.vb
@@ -0,0 +1,162 @@
+﻿Imports NUnit.Framework
+<TestFixture> Public MustInherit Class EnemyshipTest
+    Private sut As Enemyship
+    Private table As Integer()()
+    <SetUp> Public Overridable Sub Setup()
+        sut = New Enemyship
+        table = {
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
+        }
+    End Sub
+
+    Public Class CanCreateEnemyshipTest : Inherits EnemyshipTest
+
+        <Test()> Public Sub 下に向け垂直に敵船を作ろうとする際何もなければTrueと返す()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 0
+
+            Dim result As Boolean = sut.CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            Assert.IsTrue(result)
+        End Sub
+
+        <Test()> Public Sub 右に向け水平に敵船を作ろうとする際何もなければTrueと返す()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 1
+
+            Dim result As Boolean = sut.CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            Assert.IsTrue(result)
+        End Sub
+
+        <Test()> Public Sub 下に向け垂直に敵船を作ろうとする際敵船が既にあればFalse返す()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 0
+            table(1)(0) = 1
+
+            Dim result As Boolean = sut.CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            Assert.IsFalse(result)
+        End Sub
+        <Test()> Public Sub 上に向け垂直に敵船を作ろうとする際敵船が既にあればFalse返す()
+            Dim lineBeginPosition As Integer = 4
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 5
+            Dim verticallyOrHorizontally As Integer = 0
+            table(1)(0) = 1
+
+            Dim result As Boolean = sut.CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            Assert.IsFalse(result)
+        End Sub
+
+        <Test()> Public Sub 右に向け水平に敵船を作ろうとする際敵船が既にあればFalse返す()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 1
+            table(0)(1) = 1
+            Dim result As Boolean = sut.CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            Assert.IsFalse(result)
+        End Sub
+
+        <Test()> Public Sub 左に向け水平に敵船を作ろうとする際敵船が既にあればFalse返す()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 4
+            Dim enemyshipSize As Integer = 5
+            Dim verticallyOrHorizontally As Integer = 1
+            table(0)(1) = 1
+            Dim result As Boolean = sut.CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            Assert.IsFalse(result)
+        End Sub
+
+    End Class
+
+    Public Class PutInGameTableTest : Inherits EnemyshipTest
+
+        <Test()> Public Sub 下に向け垂直に敵船を配置するか()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 0
+            Dim result As Integer()() = sut.PutInGameTable(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            table = {
+            New Integer() {1, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {1, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {1, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
+        }
+            Assert.That(result, [Is].EqualTo(table))
+        End Sub
+        <Test()> Public Sub 上に向け垂直に敵船を配置するか()
+            Dim lineBeginPosition As Integer = 4
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 0
+            Dim result As Integer()() = sut.PutInGameTable(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            table = {
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {1, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {1, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {1, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
+        }
+            Assert.That(result, [Is].EqualTo(table))
+        End Sub
+        <Test()> Public Sub 右に向け水平に敵船を配置するか()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 0
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 1
+            Dim result As Integer()() = sut.PutInGameTable(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            table = {
+            New Integer() {1, 1, 1, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
+        }
+            Assert.That(result, [Is].EqualTo(table))
+        End Sub
+
+        <Test()> Public Sub 左に向け水平に敵船を配置するか()
+            Dim lineBeginPosition As Integer = 0
+            Dim columnBeginPosition As Integer = 4
+            Dim enemyshipSize As Integer = 3
+            Dim verticallyOrHorizontally As Integer = 1
+            Dim result As Integer()() = sut.PutInGameTable(lineBeginPosition, columnBeginPosition, enemyshipSize - 1, verticallyOrHorizontally, table)
+            table = {
+            New Integer() {0, 0, 1, 1, 1, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+            New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
+        }
+            Assert.That(result, [Is].EqualTo(table))
+        End Sub
+
+    End Class
+
+End Class
--- Battleship/Enemyship.vb
+++ Battleship/Enemyship.vb
@@ -3,62 +3,89 @@
         Naught = 0
         Enemy
     End Enum
+    Private Enum VerticallyOrHorizontallyList
+        Vertically = 0
+        Horizontally
+    End Enum
 
     ''' <summary>
-    ''' テーブルに敵船を作る
+    ''' 敵船を作る
     ''' </summary>
     ''' <param name="enemyshipSizes"></param>
     ''' <param name="table"></param>
     ''' <returns></returns>
     Public Function CreateEnemyship(enemyshipSizes As Integer(), table As Integer()()) As Integer()()
         Dim returnTable As Integer()() = table
-        Dim random As New Random
-        Dim i As Integer = 0
-        While i < enemyshipSizes.Length
-            Dim verticallyOrHorizontally As Integer = random.Next(2)
-            Dim lineBeginPosition As Integer = random.Next(8)
-            Dim columnBeginPosition As Integer = random.Next(8)
-            If verticallyOrHorizontally = 0 Then
-                If lineBeginPosition < 4 Then
-                    If CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSizes(i) - 1, verticallyOrHorizontally, "+"c, returnTable) Then
-                        For j As Integer = 0 To enemyshipSizes(i) - 1
-                            returnTable(lineBeginPosition + j)(columnBeginPosition) = TypeOfSquare.Enemy
-                        Next
-                    Else
-                        Continue While
-                    End If
-                Else
-                    If CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSizes(i) - 1, verticallyOrHorizontally, "-"c, returnTable) Then
-                        For j As Integer = 0 To enemyshipSizes(i) - 1
+        For Each enemyshipSize As Integer In enemyshipSizes
+            enemyshipSize -= 1
+            Dim numberNeedToEnemyship As Integer() = GetNumberNeedToMakeEnemyship(enemyshipSize, returnTable)
+            Dim verticallyOrHorizontally As Integer = numberNeedToEnemyship(0)
+            Dim lineBeginPosition As Integer = numberNeedToEnemyship(1)
+            Dim columnBeginPosition As Integer = numberNeedToEnemyship(2)
+            returnTable = PutInGameTable(lineBeginPosition, columnBeginPosition, enemyshipSize, verticallyOrHorizontally, table)
+        Next
 
-                            returnTable(lineBeginPosition - j)(columnBeginPosition) = TypeOfSquare.Enemy
-                        Next
-                    Else
-                        Continue While
-                    End If
-                End If
-            ElseIf verticallyOrHorizontally = 1 Then
-                If columnBeginPosition < 4 Then
-                    If CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSizes(i) - 1, verticallyOrHorizontally, "+"c, returnTable) Then
-                        For j As Integer = 0 To enemyshipSizes(i) - 1
-                            returnTable(lineBeginPosition)(columnBeginPosition + j) = TypeOfSquare.Enemy
-                        Next
-                    Else
-                        Continue While
-                    End If
-                Else
-                    If CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSizes(i) - 1, verticallyOrHorizontally, "-"c, returnTable) Then
-                        For j As Integer = 0 To enemyshipSizes(i) - 1
-                            returnTable(lineBeginPosition)(columnBeginPosition - j) = TypeOfSquare.Enemy
-                        Next
-                    Else
-                        Continue While
-                    End If
-                End If
-            End If
-            i += 1
-        End While
         Return returnTable
+    End Function
+
+    ''' <summary>
+    ''' テーブルに敵船を配置
+    ''' </summary>
+    ''' <param name="lineBeginPosition"></param>
+    ''' <param name="columnBeginPosition"></param>
+    ''' <param name="enemyshipSize"></param>
+    ''' <param name="verticallyOrHorizontally"></param>
+    ''' <param name="table"></param>
+    ''' <returns></returns>
+    Public Function PutInGameTable(lineBeginPosition As Integer, columnBeginPosition As Integer,
+                                          enemyshipSize As Integer, verticallyOrHorizontally As Integer, table As Integer()()) As Integer()()
+        Dim returnTable As Integer()() = table
+        If verticallyOrHorizontally = VerticallyOrHorizontallyList.Vertically Then
+            If lineBeginPosition < 4 Then
+                For i As Integer = 0 To enemyshipSize
+                    returnTable(lineBeginPosition + i)(columnBeginPosition) = TypeOfSquare.Enemy
+                Next
+            Else
+                For i As Integer = 0 To enemyshipSize
+                    returnTable(lineBeginPosition - i)(columnBeginPosition) = TypeOfSquare.Enemy
+                Next
+            End If
+        ElseIf verticallyOrHorizontally = VerticallyOrHorizontallyList.Horizontally Then
+            If columnBeginPosition < 4 Then
+                For i As Integer = 0 To enemyshipSize
+                    returnTable(lineBeginPosition)(columnBeginPosition + i) = TypeOfSquare.Enemy
+                Next
+            Else
+                For i As Integer = 0 To enemyshipSize
+                    returnTable(lineBeginPosition)(columnBeginPosition - i) = TypeOfSquare.Enemy
+                Next
+            End If
+        End If
+        Return returnTable
+    End Function
+
+    ''' <summary>
+    ''' 敵船を作る処理に必要な三つのランダム数を返す
+    ''' 配列の一つ目には、敵船を縦か、横に作るかを判断する数字
+    ''' 配列の二つ目には、敵船の行での開始位置
+    ''' 配列の三つ目には、敵船の列での開始位置
+    ''' </summary>
+    ''' <param name="enemyshipSize"></param>
+    ''' <param name="table"></param>
+    ''' <returns></returns>
+    Private Function GetNumberNeedToMakeEnemyship(enemyshipSize As Integer, table As Integer()()) As Integer()
+        Dim random As New Random
+        Dim verticallyOrHorizontally As Integer
+        Dim lineBeginPosition As Integer
+        Dim columnBeginPosition As Integer
+        Do
+            verticallyOrHorizontally = random.Next(2)
+            lineBeginPosition = random.Next(8)
+            columnBeginPosition = random.Next(8)
+        Loop While Not CanCreateEnemyship(lineBeginPosition, columnBeginPosition, enemyshipSize, verticallyOrHorizontally, table)
+        Dim numberNeedToMakeEnemyship As Integer() = {verticallyOrHorizontally, lineBeginPosition, columnBeginPosition}
+        Return numberNeedToMakeEnemyship
+
     End Function
 
     ''' <summary>
@@ -68,13 +95,12 @@
     ''' <param name="columnBeginPosition"></param>
     ''' <param name="enemyshipSize"></param>
     ''' <param name="verticallyOrHorizontally"></param>
-    ''' <param name="plusMinus"></param>
     ''' <returns></returns>
-    Private Function CanCreateEnemyship(lineBeginPosition As Integer, columnBeginPosition As Integer,
-                                          enemyshipSize As Integer, verticallyOrHorizontally As Integer, plusMinus As Char, table As Integer()()
+    Public Function CanCreateEnemyship(lineBeginPosition As Integer, columnBeginPosition As Integer,
+                                          enemyshipSize As Integer, verticallyOrHorizontally As Integer, table As Integer()()
                                           ) As Boolean
-        If verticallyOrHorizontally = 0 Then
-            If plusMinus = "+"c Then
+        If verticallyOrHorizontally = VerticallyOrHorizontallyList.Vertically Then
+            If lineBeginPosition < 4 Then
                 For i As Integer = 0 To enemyshipSize
                     If Not table(lineBeginPosition + i)(columnBeginPosition) = TypeOfSquare.Naught Then
                         Return False
@@ -88,8 +114,8 @@
                 Next
             End If
 
-        Else
-            If plusMinus = "+"c Then
+        ElseIf verticallyOrHorizontally = VerticallyOrHorizontallyList.Horizontally Then
+            If columnBeginPosition < 4 Then
                 For i As Integer = 0 To enemyshipSize
                     If Not table(lineBeginPosition)(columnBeginPosition + i) = TypeOfSquare.Naught Then
                         Return False
