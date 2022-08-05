f74792a0b9eadda4fc528baf5f30a34600f4465b
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-05 14:47:02 +0900

    処理の流れを読みやすくするためにメンバー変数の削除

--- Battleship/BattleshipGame.vb
+++ Battleship/BattleshipGame.vb
@@ -1,14 +1,5 @@
 ﻿Public Class BattleshipGame
-    Private table As Integer()() = {
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
-            New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
-        }
+
     Private ReadOnly LEFT_EDGE As Integer = 4
     Private ReadOnly TOP_EDGE As Integer = 4
 
@@ -19,15 +10,33 @@
         Miss
     End Enum
 
+    ''' <summary>
+    ''' ゲームテーブルを作る
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function CreateGameTable() As Integer()()
+        Dim table As Integer()() = {
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0},
+          New Integer() {0, 0, 0, 0, 0, 0, 0, 0}
+      }
+        Return table
+    End Function
 
     ''' <summary>
     ''' 戦艦ゲームの処理を行う
     ''' </summary>
     Public Sub BattleshipGame()
+        Dim table As Integer()() = CreateGameTable()
         Dim enemyshipSizes As Integer() = {3, 4, 5}
         Dim enemyship As New Enemyship
         table = enemyship.CreateEnemyship(enemyshipSizes, table)
-        ShowGameTable(LEFT_EDGE, TOP_EDGE)
+        ShowGameTable(LEFT_EDGE, TOP_EDGE, table)
         Console.SetCursorPosition(LEFT_EDGE, TOP_EDGE)
         Dim isGameClear As Boolean = False
         Dim attackedCount As Integer = 0
@@ -36,25 +45,25 @@
             Select Case c.Key
 
                 Case ConsoleKey.LeftArrow
-                    MoveCursor(-2, 0)
+                    MoveCursor(-2, 0, table)
 
                 Case ConsoleKey.RightArrow
-                    MoveCursor(2, 0)
+                    MoveCursor(2, 0, table)
 
                 Case ConsoleKey.UpArrow
-                    MoveCursor(0, -1)
+                    MoveCursor(0, -1, table)
 
                 Case ConsoleKey.DownArrow
-                    MoveCursor(0, 1)
+                    MoveCursor(0, 1, table)
 
                 Case ConsoleKey.Enter, ConsoleKey.Spacebar
-                    If IsNotAttackedSquare() Then
-                        AttackEnemyship()
+                    If IsNotAttackedSquare(table) Then
+                        table = AttackEnemyship(table)
                         attackedCount += 1
                     End If
 
             End Select
-            isGameClear = IsDefeatedAllTheEnemyShips()
+            isGameClear = IsDefeatedAllTheEnemyShips(table)
         End While
         Console.SetCursorPosition(0, 13)
         If Not isGameClear Then
@@ -70,8 +79,9 @@
     ''' 未攻撃マスならTrue
     ''' 攻撃済みのマスのマスならFalse
     ''' </summary>
+    ''' <param name="table">ゲームテーブル</param>
     ''' <returns></returns>
-    Private Function IsNotAttackedSquare() As Boolean
+    Private Function IsNotAttackedSquare(table As Integer()()) As Boolean
         Dim lineNumber As Integer = Console.CursorTop - 4
         Dim columnNumber As Integer = CInt((Console.CursorLeft / 2) - 2)
         Return table(lineNumber)(columnNumber) = TypeOfSquare.Enemy OrElse table(lineNumber)(columnNumber) = TypeOfSquare.Naught
@@ -81,9 +91,10 @@
     ''' <summary>
     ''' 敵船をすべて倒したか確認する
     ''' </summary>
+    ''' <param name="table">ゲームテーブル</param>
     ''' <returns>すべて倒した場合はTrue
     ''' そうでない場合はFalse</returns>
-    Private Function IsDefeatedAllTheEnemyShips() As Boolean
+    Private Function IsDefeatedAllTheEnemyShips(table As Integer()()) As Boolean
 
         For i As Integer = 0 To 7
             For j As Integer = 0 To 7
@@ -100,28 +111,33 @@
     ''' <summary>
     ''' 指定のマスに攻撃する
     ''' </summary>
-    Private Sub AttackEnemyship()
+    ''' <param name="table">ゲームテーブル</param>
+    ''' <returns></returns>
+    Private Function AttackEnemyship(table As Integer()()) As Integer()()
+        Dim returnTable As Integer()() = table
         Dim cursorLeft As Integer = Console.CursorLeft
         Dim cursorTop As Integer = Console.CursorTop
         Dim lineNumber As Integer = cursorTop - 4
         Dim columnNumber As Integer = CInt((cursorLeft / 2) - 2)
         Console.Clear()
         If table(lineNumber)(columnNumber) = TypeOfSquare.Enemy Then
-            table(lineNumber)(columnNumber) = TypeOfSquare.Attacked
+            returnTable(lineNumber)(columnNumber) = TypeOfSquare.Attacked
         ElseIf table(lineNumber)(columnNumber) = TypeOfSquare.Naught Then
-            table(lineNumber)(columnNumber) = TypeOfSquare.Miss
+            returnTable(lineNumber)(columnNumber) = TypeOfSquare.Miss
         End If
 
-        ShowGameTable(cursorLeft, cursorTop)
+        ShowGameTable(cursorLeft, cursorTop, returnTable)
         Console.SetCursorPosition(cursorLeft, cursorTop)
-    End Sub
+        Return returnTable
+    End Function
 
     ''' <summary>
     ''' ゲームテーブルを表示する
     ''' </summary>
     ''' <param name="cursorLeft">X座標でのカーソルの位置</param>
     ''' <param name="cursorTop">Y座標でのカーソルの位置</param>
-    Private Sub ShowGameTable(cursorLeft As Integer, cursorTop As Integer)
+    ''' <param name="table">ゲームテーブル</param>
+    Private Sub ShowGameTable(cursorLeft As Integer, cursorTop As Integer, table As Integer()())
         Console.WriteLine("【BATTLESHIP】")
         Dim columnArrow As String = MakeColumnArrow(cursorLeft)
         Console.WriteLine(columnArrow)
@@ -168,7 +184,8 @@
     ''' </summary>
     ''' <param name="x">X座標を移動する値</param>
     ''' <param name="y">Y座標を移動する値</param>
-    Private Sub MoveCursor(x As Integer, y As Integer)
+    ''' <param name="table">ゲームテーブル</param>
+    Private Sub MoveCursor(x As Integer, y As Integer, table As Integer()())
         Dim afterCursorLeft As Integer = Console.CursorLeft + x
         Dim afterCursorTop As Integer = Console.CursorTop + y
         Const RIGHT_EDGE As Integer = 18
@@ -188,7 +205,7 @@
         End If
 
         Console.Clear()
-        ShowGameTable(afterCursorLeft, afterCursorTop)
+        ShowGameTable(afterCursorLeft, afterCursorTop, table)
         'カーソルの位置を移動させる
         Console.SetCursorPosition(afterCursorLeft, afterCursorTop)
 
