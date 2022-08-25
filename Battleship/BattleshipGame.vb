b65dc5fe1667b3ad169b1df6376e4b36e65560a0
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-08-26 08:53:14 +0900

    マスの種類を持つEnumを追加

---
+++ Battleship/BattleshipGame.vb
@@ -0,0 +1,17 @@
+﻿Public Class BattleshipGame
+
+    ''' <summary>
+    ''' マスの種類
+    ''' </summary>
+    Public Enum TypeOfSquare
+        ''' <summary>空マス</summary>
+        Naught = 0
+        ''' <summary>敵マス</summary>
+        Enemy
+        ''' <summary>攻撃済みマス</summary>
+        Attacked
+        ''' <summary>攻撃失敗マス</summary>
+        Miss
+    End Enum
+
+End Class
